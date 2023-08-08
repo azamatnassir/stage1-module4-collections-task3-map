a7d36728dba0f30f0ea9dbe524b61f6537b37d3d
azamatnassir <azamatnsr@gmail.com>
2023-08-08 17:21:33 +0600

    commit

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,20 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        HashMap<String, Integer> rev = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet())
+            if (rev.containsKey(entry.getValue())) {
+                if (rev.get(entry.getValue()) > entry.getKey()) {
+                    rev.replace(entry.getValue(), entry.getKey());
+                }
+            } else {
+                rev.put(entry.getValue(), entry.getKey());
+            }
+
+        return rev;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,22 @@
 package com.epam.mjc.collections.map;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> freq = new HashMap<>();
+
+        for (String s : List.of(Arrays.stream(sentence.toLowerCase().split("[,. ]")).filter(e -> !e.trim().isEmpty()).toArray(String[]::new))) {
+            int prev = 0;
+            if (freq.get(s) != null) {
+                prev = freq.get(s);
+            }
+            freq.put(s, prev + 1);
+        }
+
+        return freq;
     }
 }
